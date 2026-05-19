855c5921ceb1dccd5aff794ff8a783c8670c86f7
Nihal Agarwal <theexplorer1410@gmail.com>
2026-05-19 13:21:53 +0530

    Added delete book feature

--- main.cpp
+++ main.cpp
@@ -88,6 +88,54 @@ public:
 
         file.close();
     }
+
+    void deleteBook()
+    {
+        ifstream file("books.txt");
+
+        ofstream tempFile("temp.txt");
+
+        int deleteID;
+
+        bool found = false;
+
+        cout << "\nEnter Book ID to Delete: ";
+        cin >> deleteID;
+
+        while(file >> bookID)
+        {
+            file.ignore();
+
+            getline(file, title, '|');
+            getline(file, author);
+
+            if(bookID == deleteID)
+            {
+                found = true;
+
+                continue;
+            }
+
+            tempFile << bookID << "|"
+                     << title << "|"
+                     << author << endl;
+        }
+
+        file.close();
+        tempFile.close();
+
+        remove("books.txt");
+        rename("temp.txt", "books.txt");
+
+        if(found)
+        {
+            cout << "\nBook Deleted Successfully!\n";
+        }
+        else
+        {
+            cout << "\nBook Not Found!\n";
+        }
+    }
 };
 
 int main()
@@ -103,7 +151,8 @@ int main()
         cout << "1. Add Book\n";
         cout << "2. Display Books\n";
         cout << "3. Search Book\n";
-        cout << "4. Exit\n";
+        cout << "4. Delete Book\n";
+        cout << "5. Exit\n";
 
         cout << "Enter Choice: ";
         cin >> choice;
@@ -123,6 +172,10 @@ int main()
                 break;
 
             case 4:
+                b.deleteBook();
+                break;
+
+            case 5:
                 cout << "\nExiting Program...\n";
                 break;
 
@@ -130,7 +183,7 @@ int main()
                 cout << "\nInvalid Choice!\n";
         }
 
-    } while(choice != 4);
+    } while(choice != 5);
 
     return 0;
-}git add .
+}
